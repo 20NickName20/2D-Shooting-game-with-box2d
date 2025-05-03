ea89e1f1b4d79d8458accc259d9442f5c7d1caed
XAWENT <XAWENT@yandex.ru>
2025-05-03 23:42:43 +0300

    Add sniperGun :3

---
+++ core/src/main/java/io/github/_20nickname20/imbored/game_objects/items/usable/guns/raycast/SniperGunItem.java
@@ -0,0 +1,33 @@
+package io.github._20nickname20.imbored.game_objects.items.usable.guns.raycast;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import io.github._20nickname20.imbored.game_objects.Entity;
+import io.github._20nickname20.imbored.game_objects.entities.living.human.CursorEntity;
+import io.github._20nickname20.imbored.game_objects.entities.living.human.cursor.PlayerEntity;
+import io.github._20nickname20.imbored.game_objects.items.usable.guns.RaycastGunItem;
+
+public class SniperGunItem extends RaycastGunItem {
+    public SniperGunItem(Entity holder) {
+        super(holder, 2f, 0.7f, 15.5f,1,1, 34f, 0.09f, 370f, MathUtils.degRad * 15, 1.275f);
+    }
+
+    @Override
+    public void render(ShapeRenderer renderer, CursorEntity handHolder) {
+        super.render(renderer, handHolder);
+        renderer.setColor(0.2f, 0.3f, 0.0f, 1);
+        renderer.rectLine(-0.75f, -0.75f, 0.1f, 0.1f, 0.8f);
+        renderer.rectLine(0, 0, 6.5f, 0, 0.9f);
+        renderer.rectLine(3.0f, 0.7f, 0.3f, 0.7f, 0.5f);
+    }
+
+    @Override
+    public void onStartUse(PlayerEntity player) {
+        shootAttempt(player);
+    }
+
+    @Override
+    public void onEndUse(PlayerEntity player) {
+
+    }
+}
